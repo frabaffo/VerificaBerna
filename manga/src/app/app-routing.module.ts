296d2b0e972575867492a7109a9cd78592cc375b
frabaffo <banfi.francesco@iisgalvanimi.edu.it>
2021-01-12 14:25:42 +0000

    punto 3

--- manga/src/app/app-routing.module.ts
+++ manga/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchMangaComponent } from './search-manga/search-manga.component';
+import { MangaComponent } from './manga/manga.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'manga/:id', component: MangaComponent },
+  { path: 'search', component: SearchMangaComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- manga/src/app/app.module.ts
+++ manga/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MangaComponent } from './manga/manga.component';
+import { SearchMangaComponent } from './search-manga/search-manga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MangaComponent,
+    SearchMangaComponent
   ],
   imports: [
     BrowserModule,
